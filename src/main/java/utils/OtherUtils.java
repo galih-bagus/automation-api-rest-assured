f9dfbc05d216f23df6021cb87ee78579c7ca3aec
galih-bagus <galihbagus555@gmail.com>
2025-01-06 20:07:40 +0700

    add validate json schema

--- src/main/java/utils/OtherUtils.java
+++ src/main/java/utils/OtherUtils.java
@@ -4,45 +4,54 @@ import Helper.Config;
 import io.restassured.RestAssured;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 import java.io.File;
 
 
 public class OtherUtils {
-    public static Response responseGet(String baseUrl, String token, Integer statusCode, String jsonSchemaPath) {
+    public static Response responseGet(String baseUrl, String token, Integer statusCode, String jsonSchemaPath, Boolean isValidateJson) {
         File schemaFile = new File("src/test/resources/" + jsonSchemaPath);
         if (!schemaFile.exists()) {
             throw new IllegalArgumentException("JSON Schema tidak ditemukan di path: " + jsonSchemaPath);
         }
-        Response response = RestAssured.given()
+        ValidatableResponse validatableResponse = RestAssured.given()
                 .baseUri(Config.get("baseUrl"))
                 .header("Authorization", "Bearer " + token)
                 .when()
                 .get(baseUrl)
-                .then()
-                .assertThat().body(JsonSchemaValidator.matchesJsonSchema(schemaFile))
-                .statusCode(statusCode)
+                .then();
+
+        if (isValidateJson) {
+            validatableResponse = validatableResponse.assertThat().body(JsonSchemaValidator.matchesJsonSchema(schemaFile));
+        }
+
+        Response response = validatableResponse.statusCode(statusCode)
                 .extract()
                 .response();
 
         return response;
     }
 
-    public static Response responsePost(String baseUrl, String token, String requestBody, Integer statusCode, String jsonSchemaPath) {
+    public static Response responsePost(String baseUrl, String token, String requestBody, Integer statusCode, String jsonSchemaPath, Boolean isValidateJson) {
         File schemaFile = new File("src/test/resources/" + jsonSchemaPath);
         if (!schemaFile.exists()) {
             throw new IllegalArgumentException("JSON Schema tidak ditemukan di path: " + jsonSchemaPath);
         }
-        Response response = RestAssured.given()
+        ValidatableResponse validatableResponse = RestAssured.given()
                 .contentType("application/json")
                 .baseUri(Config.get("baseUrl"))
                 .header("Authorization", "Bearer " + token)
                 .when()
                 .body(requestBody)
                 .post(baseUrl)
-                .then()
-                .assertThat().body(JsonSchemaValidator.matchesJsonSchema(schemaFile))
-                .statusCode(statusCode)
+                .then();
+
+        if (isValidateJson) {
+            validatableResponse = validatableResponse.assertThat().body(JsonSchemaValidator.matchesJsonSchema(schemaFile));
+        }
+
+        Response response = validatableResponse.statusCode(statusCode)
                 .extract()
                 .response();
 
