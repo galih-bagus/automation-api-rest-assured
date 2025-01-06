6831f607743bde2761a6654e4b981a1a2d75c441
galih-bagus <galihbagus555@gmail.com>
2025-01-06 20:32:44 +0700

    add : scenario add release

--- src/test/java/release/ReleaseTest.java
+++ src/test/java/release/ReleaseTest.java
@@ -1,6 +1,8 @@
 package release;
 
+import Data.releaseData;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.AuthUtils;
@@ -19,5 +21,17 @@ public class ReleaseTest {
         Assert.assertEquals(message, "success", "Pesan tidak sesuai!");
         Assert.assertNotNull("Data tidak ditemukan!", data);
 
+    }
+
+    @Test(priority = 2)
+    public void createRelease() {
+        String token = AuthUtils.login();
+        JSONObject requestBody = releaseData.create(this.createdProjectId);
+        String body = requestBody.toString();
+        Response response = OtherUtils.responsePost("/crud/release", token, body, 201, "project-schema/create-project-schema.json", true);
+        String data = response.asString();
+        String message = response.jsonPath().getString("message");
+        Assert.assertEquals(message, "success", "Pesan tidak sesuai!");
+        Assert.assertNotNull("Data tidak ditemukan!", data);
     }
 }
