321b8da7733eca1ddcf0b62d5ec5dd5650fbf244
galih-bagus <galihbagus555@gmail.com>
2025-01-06 21:13:42 +0700

    add : data update

--- src/main/java/Data/releaseData.java
+++ src/main/java/Data/releaseData.java
@@ -13,14 +13,32 @@ public class releaseData {
     public static JSONObject create(String projectId) {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        DateTimeFormatter dateNow = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime dateEnd = now.plusDays(7);
-        DateTimeFormatter dateEndFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String formattedNow = now.format(formatter);
-        String formattedDateNow = now.format(dateNow);
-        String formattedDateEnd = dateEnd.format(dateNow);
+        String formattedDateNow = now.format(formatterDate);
+        String formattedDateEnd = dateEnd.format(formatterDate);
         Map<String, Object> requestBody = new HashMap<>();
         requestBody.put("name", "Automation API Rest Assured " + formattedNow);
+        requestBody.put("start_date", formattedDateNow);
+        requestBody.put("end_date", formattedDateEnd);
+        requestBody.put("project_id", projectId);
+        JSONObject jsonObject = new JSONObject(requestBody);
+
+        return jsonObject; // Return the JSONObject
+    }
+
+    public static JSONObject update(String projectId) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime dateStart = now.plusDays(7);
+        LocalDateTime dateEnd = now.plusDays(14);
+        String formattedNow = now.format(formatter);
+        String formattedDateNow = dateStart.format(formatterDate);
+        String formattedDateEnd = dateEnd.format(formatterDate);
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("name", "Automation API Rest Assured Update " + formattedNow);
         requestBody.put("start_date", formattedDateNow);
         requestBody.put("end_date", formattedDateEnd);
         requestBody.put("project_id", projectId);
